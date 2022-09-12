1ed0aae3744afc86e0325df04a2c6acb7fbc45b1
manishmaheshwari99 <manish.maheshwari@aurigait.com>
2022-09-12 23:25:22 +0530

    add todo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import store from './store'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(Vuex)
+.use(store)
+.mount('#app')
